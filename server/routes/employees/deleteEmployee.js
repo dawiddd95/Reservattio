3540b54297bf7b48d6c5a709f6e3fdbec56afb6e
dawiddd95 <dawlyc@wp.pl>
2020-03-30 18:11:50 +0200

    update deleteEmployee route

--- server/routes/employees/deleteEmployee.js
+++ server/routes/employees/deleteEmployee.js
@@ -10,6 +10,13 @@ const Op = Sequelize.Op;
 router.post('/api/user/employees/delete', checkToken, async (req, res) => { 
    const {id} = req.body;
 
+   const reservation = await models.Reservation.findOne({ where: {employeeId: id} })
+
+   if(reservation) res.json({
+      success: false, 
+      error: 'Employee has reservations assigned to him. Please first delete all reservations assign to this employee.'
+   })
+
    try {
       models.Employee.destroy({where: {
          id: {
